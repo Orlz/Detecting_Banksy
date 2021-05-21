df9a0abdc2c080a467a40a3eb07414bf86b079ad
Orlz <cds-au604588@chcaa-worker02.uni.au.dk>
2021-05-21 09:43:16 +0200

    Fixing VE bugs

--- create_virtual_environment.sh
+++ create_virtual_environment.sh
@@ -9,9 +9,13 @@ pip install --upgrade pip
 pip install ipython
 pip install jupyter
 pip install tensorflow
+pip install matplotlib
+pip install opencv-python
+pip install pydot
+pip install tqdm
 python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements.txt && pip install -r requirements.txt
+test -f requirements.txt && pip install requirements.txt
 
 deactivate
 echo "build $VENVNAME"
